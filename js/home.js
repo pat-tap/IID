50f40bae5996865b43bdbaa3f4abfa9150db1a38
gitDew <krisztian.harmat@gmail.com>
2016-12-10 18:46:54 +0100

    Elemente können hinzugefügt werden
    Keine änderung der attributes, karotten werden als veranschaulichung
    hinzugefügt.

--- js/home.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-	$(".close").click(function() {
-		var id = $(this).parent().attr('id');
-		$('#' + id).remove();
-		$('#alert-count').html(parseInt($('#alert-count').html(), 10) - 1);
-	});
-
-});
